6fe4127424959474e06fe8ee40094decbbedc469
OronAvisrur <oronavisrur@gmail.com>
2026-01-30 12:08:51 +0200

    Add DexiosToken ERC-20 contract

---
+++ contracts/DexiosToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DexiosToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Dexios Token", "DXIO") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
