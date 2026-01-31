4c4056f7c9cb3935c1192fd09efb383f2b099854
OronAvisrur <oronavisrur@gmail.com>
2026-01-31 18:32:48 +0200

    feat(frontend): integrate toast system into App
    - Add useToast hook for global state
    - Create ToastContainer wrapper
    - Pass showToast to all route components
    - Enable real-time notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,35 @@
-import React, { useState } from 'react';
-import './App.css';
-import useWeb3 from './hooks/useWeb3';
-import useContracts from './hooks/useContracts';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Marketplace from './components/Marketplace';
 import CreateGig from './components/CreateGig';
 import MyGigs from './components/MyGigs';
 import MyOrders from './components/MyOrders';
+import ToastContainer from './components/ToastContainer';
+import { useToast } from './hooks/useToast';
+import './App.css';
 
 function App() {
-  const { web3, account, connectWallet, isConnected } = useWeb3();
-  const { marketplace, token, nft, loading } = useContracts(web3);
-  const [currentView, setCurrentView] = useState('home');
-
-  const renderView = () => {
-    if (!isConnected) {
-      return <Home connectWallet={connectWallet} />;
-    }
-
-    if (loading) {
-      return <div className="loading">Loading contracts...</div>;
-    }
-
-    switch (currentView) {
-      case 'marketplace':
-        return <Marketplace marketplace={marketplace} token={token} account={account} />;
-      case 'create-gig':
-        return <CreateGig marketplace={marketplace} nft={nft} account={account} setView={setCurrentView} />;
-      case 'my-gigs':
-        return <MyGigs marketplace={marketplace} account={account} />;
-      case 'my-orders':
-        return <MyOrders marketplace={marketplace} token={token} account={account} />;
-      default:
-        return <Home connectWallet={connectWallet} />;
-    }
-  };
+  const { toasts, showToast, removeToast } = useToast();
 
   return (
-    <div className="App">
-      <Navbar 
-        account={account} 
-        isConnected={isConnected} 
-        setView={setCurrentView}
-        currentView={currentView}
-      />
-      <div className="container">
-        {renderView()}
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/marketplace" element={<Marketplace showToast={showToast} />} />
+            <Route path="/create-gig" element={<CreateGig showToast={showToast} />} />
+            <Route path="/my-gigs" element={<MyGigs showToast={showToast} />} />
+            <Route path="/my-orders" element={<MyOrders showToast={showToast} />} />
+          </Routes>
+        </div>
+        <ToastContainer toasts={toasts} removeToast={removeToast} />
       </div>
-    </div>
+    </Router>
   );
-}
+};
 
 export default App;
